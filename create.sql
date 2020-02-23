8988b9de898f1b5fcc06276b4bafc3dc141ebebe
apethani21 <ajaypethani97@gmail.com>
2020-02-23 14:41:13 +0000

    comment clarifying timestamp check

--- create.sql
+++ create.sql
@@ -3,6 +3,6 @@ CREATE TABLE WEIGHT(
     datetime TEXT NOT NULL,
     weight REAL NOT NULL,
     loss REAL NOT NULL,
-    CHECK (timestamp > 1577836800 AND
+    CHECK (timestamp > 1577836800 AND -- 1577836800 = 01/01/2020 00:00:00
            weight > 0)
 );
