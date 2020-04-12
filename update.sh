13e0139c352cbbf51a4b5a3839c0b8177cb7f868
apethani21 <ajaypethani97@gmail.com>
2020-04-12 10:00:25 +0000

    updated to work on aws server

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
-/home/ubuntu/miniconda3/envs/weight-loss-dev/bin/python -u update_db.py >> /home/ubuntu/logs/weight-db-update.log 2>&1;
-/home/ubuntu/miniconda3/envs/weight-loss-dev/bin/jupyter nbconvert --to notebook --execute /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.ipynb >> /home/ubuntu/logs/weight-loss-analysis.log 2>&1;
+/home/ubuntu/anaconda3/envs/weight-loss-dev/bin/python -u update_db.py >> /home/ubuntu/logs/weight-db-update.log 2>&1;
+/home/ubuntu/anaconda3/envs/weight-loss-dev/bin/jupyter nbconvert --to notebook --execute /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.ipynb >> /home/ubuntu/logs/weight-loss-analysis.log 2>&1;
 mv /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.nbconvert.ipynb /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.ipynb;
