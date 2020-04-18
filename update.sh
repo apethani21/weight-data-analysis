508d4ed4e4e759c8ba09f7f3d12677323f841385
apethani21 <ajaypethani97@gmail.com>
2020-04-18 14:04:19 +0100

    sqlite3 to postgres

--- update.sh
+++ update.sh
@@ -1,3 +1,13 @@
+printf "running update_db.py\n"
 /home/ubuntu/anaconda3/envs/weight-loss-dev/bin/python -u update_db.py >> /home/ubuntu/logs/weight-db-update.log 2>&1;
-/home/ubuntu/anaconda3/envs/weight-loss-dev/bin/jupyter nbconvert --to notebook --execute /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.ipynb >> /home/ubuntu/logs/weight-loss-analysis.log 2>&1;
-mv /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.nbconvert.ipynb /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.ipynb;
+printf "successfully updated database\n";
+printf "executing weight-loss-analysis.ipynb..\n";
+/home/ubuntu/anaconda3/envs/weight-loss-dev/bin/jupyter nbconvert \
+    --to notebook \
+    --execute \
+    /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.ipynb >> \
+    /home/ubuntu/logs/weight-loss-analysis.log 2>&1 && \
+    printf "successfully executed weight-loss-analysis.ipynb\n" && \
+    mv /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.nbconvert.ipynb \
+    /home/ubuntu/repo/weight-data-analysis/weight-loss-analysis.ipynb && \
+    printf "weight-loss-analysis.ipynb updated\n";
